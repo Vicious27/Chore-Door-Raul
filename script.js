33823b4d25fd68e03bd6d114b7d5de94eca0070a
Raul Garcia <punkvomit21@gmail.com>
2022-01-26 19:19:43 -0500

    creates a function to ramdomize where the choreBot door is

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ const doorImage1 = document.getElementById('door1');
 const doorImage2 = document.getElementById('door2');
 const doorImage3 = document.getElementById('door3');
 
+const numClosedDoors = 3;
+
+// img URL's
 const botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg';
 
 const beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg';
 
 const spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg';
 
+
+
+//function for randomizing the choreBot
+const randomChoreDoorGenerator = () => {
+  const choreDoor = Math.floor(Math.random() * numClosedDoors);
+};
+
+
+// onclick functions
 doorImage1.onclick = () => {
   doorImage1.src = botDoorPath;
 };
